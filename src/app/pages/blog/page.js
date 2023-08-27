c9ab795d73b8dd79bf4e9463d11eb06b050ce43c
rayyanekaputra <bullzhit99@gmail.com>
2023-08-27 18:01:31 +0800

    chore: added placeholder page, no code change

---
+++ src/app/pages/blog/page.js
@@ -0,0 +1,29 @@
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+import React from "react";
+
+
+const Blog = () => {
+
+  
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <main>
+        {/* https://rapidapi.com/guides/how-to-use-fetch-api-in-next-js */}
+        {/* https://www.freecodecamp.org/news/how-to-consume-rest-apis-in-react/ */}
+        <button >Make API call</button>
+      
+
+      </main>
+    </div>
+  );
+};
+
+export default Blog;
