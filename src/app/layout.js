27b45825a0e3fbd971e6f6b3c0c3abdc91ceade3
rayyanekaputra <bullzhit99@gmail.com>
2023-08-28 16:00:41 +0800

    fix: font inter not rendered properly on vercel (10)

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import Navbar from '@components/navigation/navbar/navbar'
 import Footer from '@components/navigation/footer/footer'
-import '@styles/global-styles.css'
+import globalStyles from '@styles/global-styles.css'
+import {Inter} from "next/font/google"
+
+const inter = Inter({subsets:['latin']})
 
 export const metadata = {
   title: 'UBICON',
@@ -10,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body style={inter.globalStyles}>
         <Navbar/>
           {children}
         <Footer/>
