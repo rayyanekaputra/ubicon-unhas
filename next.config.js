ceb5b1f6699843875425e1cc713542f8464e8867
rayyanekaputra <bullzhit99@gmail.com>
2023-08-30 01:36:23 +0800

    fix: added API new import location from URL

--- next.config.js
+++ next.config.js
@@ -2,3 +2,17 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+// https://nextjs.org/docs/messages/next-image-unconfigured-host
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'ubi-api.onrender.com',
+          port: '',
+          pathname: '/news/thumbnail/**',
+        },
+      ],
+    },
+  }
