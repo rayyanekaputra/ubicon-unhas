c81ed8ca442c0a9636157e91927e83f75f49de5e
rayyanekaputra <bullzhit99@gmail.com>
2023-09-05 11:56:33 +0800

    feat: added loading page for home and blog

---
+++ src/app/loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    }}>Loading</div>
+  )
+}
