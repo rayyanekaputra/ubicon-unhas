b648db0a47012642ff6f9695805e02638015b380
rayyanekaputra <bullzhit99@gmail.com>
2023-09-05 11:58:30 +0800

    added: dynamic page to each post with API fetch

---
+++ src/app/blog/posts/[id]/page.js
@@ -0,0 +1,113 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import "@styles/pages/blog/news_detail/news_detail.scss";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import Image from "next/image";
+const page = ({ params }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(`https://ubi-api.onrender.com/news`);
+
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        let actualData = await response.json();
+        console.log(actualData);
+        let actualDataInside = actualData.data;
+        setData(actualDataInside);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+        //https://www.w3schools.com/jsref/jsref_try_catch.asp
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
+  // filter data, return dari filter() itu array baru
+  const dataNewForIdNews = data.filter((item) => item.id_news === params.id);
+  console.log(dataNewForIdNews, "ini data baru");
+
+  // Split the text based on dots and create an array
+  const splitKonten =
+    dataNewForIdNews.length > 0
+      ? dataNewForIdNews[0].konten.split(/[.!?]/)
+      : [];
+  console.log(splitKonten, "ini split konten");
+  const handleShowLoading = () => {
+    setLoading(true);
+  };
+  return (
+    <div>
+      {/* 
+      Troubleshoot skeleton manual UI
+      <button onClick={handleShowLoading}> SHOW LOADING </button> 
+      */}
+
+      {loading && (
+        <section className="news-body loading">
+          <div className="news-body-content-header">
+            <div className="h1"></div>
+            <div className="h1 besardikit"></div>
+            <div className="date-wrapper">
+              <CalendarMonthIcon />
+            </div>
+          </div>
+          <div className="img-news-container">
+
+          </div>
+          <div className="news-body-content">
+            <div className="h1"></div>
+            <div className="h1 besardikit"></div>
+            <div className="h1 besardikit"></div>
+            <div className="h1"></div>
+            <div className="h1 besardikit"></div>
+          </div>
+          
+        </section>
+      )}
+      {error && <p>Error: {error}</p>}
+     
+      {dataNewForIdNews.length > 0 && (
+        <section className="news-body">
+          <div className="news-body-content-header">
+            <h1>{dataNewForIdNews[0].judul}</h1>
+            <div className="date-wrapper">
+              <CalendarMonthIcon />
+              <p>{dataNewForIdNews[0].tanggal}</p>
+            </div>
+          </div>
+          <div className="img-news-container">
+            <Image
+              src={dataNewForIdNews[0].thumbnail}
+              alt={dataNewForIdNews[0].alt}
+              fill
+            />
+          </div>
+
+          <p>
+            {splitKonten.map((part, index) => (
+              <span key={index}>
+                {part}
+                <br /> {/* Render a new line */}
+              </span>
+            ))}
+          </p>
+        </section>
+      )}
+    </div>
+  );
+};
+
+export default page;
